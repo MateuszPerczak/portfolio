d6075c335297e7886bfdfb8b2aab5a081cd614e6
Mateusz Perczak <40388335+losek1@users.noreply.github.com>
2022-05-21 23:56:44 +0200

    Deleted legacy content

--- scripts/consoleInit.js
+++
@@ -1,8 +0,0 @@
-const consoleInit = () => {
-  console.log(
-    "%c+",
-    `font-size:1px; background:url('${window.location.origin}/img/consoleImage.png') no-repeat; padding: 300px 300px;`
-  );
-};
-
-export default consoleInit;
