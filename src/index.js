4815621b111e2752a099ab0336a5d76142116800
Mateusz Perczak <40388335+losek1@users.noreply.github.com>
2022-06-07 02:59:43 +0200

    Temporarily removed react strict mode because of a bug.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import "./i18n";
 import greatings from "./Hooks/console";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 greatings();
