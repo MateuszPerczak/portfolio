16f4dc66397177be8817ab3700abf2872c838a53
Mateusz Perczak <40388335+losek1@users.noreply.github.com>
2022-07-19 21:10:14 +0200

    Fixed font reloading

--- src/Components/Icon/Icon.tsx
+++ src/Components/Icon/Icon.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 import IconComponent from "./Icon.types";
 
 const Icon: IconComponent = styled.span`
-  font-family: "Segoe Fluent Icons", sans-serif;
+  font-family: "Segoe Fluent Icons", "Segoe Fluent Icons", "Segoe MDL2 Assets";
 `;
 
 export default Icon;
