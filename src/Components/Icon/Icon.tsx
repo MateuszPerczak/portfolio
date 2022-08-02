96ab2c68ebe7cb1411f8ba1c79f2b6fe25c3ade1
Mateusz Perczak <40388335+losek1@users.noreply.github.com>
2022-08-02 17:23:19 +0200

    created vite template

--- src/Components/Icon/Icon.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-import IconComponent from "./Icon.types";
-
-const Icon: IconComponent = styled.span`
-  font-family: "Segoe Fluent Icons", sans-serif;
-`;
-
-export default Icon;
--- src/Contexts/ThemeContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import { Themes } from "../Hooks/useTheme";
-
-const ThemeContext = createContext({
-  browserTheme: Themes.System,
-  setbrowserTheme: (newTheme: Themes) => {},
-});
-
-export default ThemeContext;
