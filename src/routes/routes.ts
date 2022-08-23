80dbc25b45c2b73afa81d916f14d7eeafc86d32b
Mateusz Perczak <40388335+losek1@users.noreply.github.com>
2022-08-23 22:48:17 +0200

    Changes to be committed: 	modified:   src/routes/routes.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,35 +1,108 @@
-import type { FC, LazyExoticComponent } from "react";
-import { lazy } from "react";
+import { FC, lazy, LazyExoticComponent } from "react";
 
-const About: LazyExoticComponent<FC> = lazy(() => import("../pages/About"));
-const Skills: LazyExoticComponent<FC> = lazy(() => import("../pages/Skills"));
-const Projects: LazyExoticComponent<FC> = lazy(() => import("../pages/Projects"));
+const AboutMe: LazyExoticComponent<FC> = lazy(
+  (): Promise<typeof import("../Pages/AboutMe")> => {
+    return new Promise((resolve) => {
+      resolve(import("../Pages/AboutMe"));
+    });
+  }
+);
 
-type Route = {
-  id: number;
+const Skills: LazyExoticComponent<FC> = lazy(
+  (): Promise<typeof import("../Pages/Skills")> => {
+    return new Promise((resolve) => {
+      resolve(import("../Pages/Skills"));
+    });
+  }
+);
+
+const Preferences: LazyExoticComponent<FC> = lazy(
+  (): Promise<typeof import("../Pages/Preferences")> => {
+    return new Promise((resolve) => {
+      resolve(import("../Pages/Preferences"));
+    });
+  }
+);
+
+const Contact: LazyExoticComponent<FC> = lazy(
+  (): Promise<typeof import("../Pages/Contact")> => {
+    return new Promise((resolve) => {
+      resolve(import("../Pages/Contact"));
+    });
+  }
+);
+
+const Projects: LazyExoticComponent<FC> = lazy(
+  (): Promise<typeof import("../Pages/Projects")> => {
+    return new Promise((resolve) => {
+      resolve(import("../Pages/Projects"));
+    });
+  }
+);
+
+const Experience: LazyExoticComponent<FC> = lazy(
+  (): Promise<typeof import("../Pages/Experience")> => {
+    return new Promise((resolve) => {
+      resolve(import("../Pages/Experience"));
+    });
+  }
+);
+
+type routeType = {
   path: string;
+  component: FC;
+  icon: string;
   name: string;
-  element: FC;
+  position: RoutePosition;
 };
 
-const routes: Route[] = [
+export enum RoutePosition {
+  Top = "top",
+  Bottom = "bottom",
+}
+
+const routes: routeType[] = [
   {
-    id: 1,
     path: "/",
-    name: "O MNIE",
-    element: About,
+    component: AboutMe,
+    icon: "\uE13D",
+    name: "aboutMe",
+    position: RoutePosition.Top,
   },
   {
-    id: 2,
     path: "/skills",
-    name: "UMIEJĘTNOŚCI",
-    element: Skills,
+    component: Skills,
+    icon: "\uE7BE",
+    name: "skills",
+    position: RoutePosition.Top,
   },
   {
-    id: 3,
+    path: "/experience",
+    component: Experience,
+    icon: "\uE821",
+    name: "experience",
+    position: RoutePosition.Top,
+  },
+  {
     path: "/projects",
-    name: "PROJEKTY",
-    element: Projects,
+    component: Projects,
+    icon: "\uE74C",
+    name: "projects",
+    position: RoutePosition.Top,
+  },
+  {
+    path: "/contact",
+    component: Contact,
+    icon: "\uE715",
+    name: "contact",
+    position: RoutePosition.Top,
+  },
+  {
+    path: "/preferences",
+    component: Preferences,
+    icon: "\uE15E",
+    name: "preferences",
+    position: RoutePosition.Bottom,
   },
 ];
 
