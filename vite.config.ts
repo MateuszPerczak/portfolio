0da4107ccc561ba819ac175c5fab419f8b9b0675
Mateusz Perczak <40388335+losek1@users.noreply.github.com>
2022-08-29 23:02:52 +0200

    Removed base name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "mateuszperczak.github.io",
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
