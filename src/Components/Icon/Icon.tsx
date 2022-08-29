a890741612dae782e3da518302a078594c1a1e51
Mateusz Perczak <40388335+losek1@users.noreply.github.com>
2022-08-29 22:36:45 +0200

    Preparing for vite site

--- src/Components/Icon/Icon.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-import { memo } from "react";
-import IconComponent from "./Icon.types";
-
-const Icon: IconComponent = styled.span`
-  font-family: "Segoe Fluent Icons", "Segoe Fluent Icons";
-`;
-
-export default memo(Icon);
