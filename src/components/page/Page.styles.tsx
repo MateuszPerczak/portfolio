1d2e6d37e43f1305e33411cad7a96581e83e8e2b
Mateusz Perczak <40388335+losek1@users.noreply.github.com>
2022-09-11 23:49:33 +0200

    Fixed article to main

--- src/components/page/Page.styles.tsx
+++ src/components/page/Page.styles.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import { motion } from "framer-motion";
 
-const StyledPage = styled(motion.article)`
+const StyledPage = styled(motion.main)`
   display: flex;
   flex-direction: column;
   align-items: center;
