8c9c0101a660adf22b7ee9842e2e883ec3103922
Mateusz Perczak <40388335+losek1@users.noreply.github.com>
2022-09-29 19:53:35 +0200

    Fixed loader

--- src/components/loader/Loader.styles.tsx
+++ src/components/loader/Loader.styles.tsx
@@ -2,11 +2,17 @@ import styled from "@emotion/styled";
 import { motion } from "framer-motion";
 
 const StyledLoader = styled.article`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 2;
   display: flex;
   justify-content: center;
   align-items: center;
-  flex: 1 1 100%;
   overflow: hidden;
+  background-color: ${({ theme: { accent } }) => accent};
 `;
 
 export const StyledLoaderElement = styled(motion.div)`
@@ -15,7 +21,7 @@ export const StyledLoaderElement = styled(motion.div)`
   align-items: center;
   font-size: 3rem;
   flex: 1 1 100%;
-  color: ${({ theme: { accent } }) => accent};
+  color: ${({ theme: { color } }) => color};
 `;
 
 export default StyledLoader;
