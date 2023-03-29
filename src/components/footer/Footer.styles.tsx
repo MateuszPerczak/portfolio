3cf608c7d4c572b06136380f6c4bfdc3deacc06a
Mateusz Perczak <mateusz.perczak.mp@gmail.com>
2023-03-29 22:21:07 +0200

    Added footer

--- src/components/footer/Footer.styles.tsx
+++ src/components/footer/Footer.styles.tsx
@@ -2,11 +2,39 @@ import styled from "@emotion/styled";
 
 const StyledFooter = styled.footer`
   display: flex;
-  flex: 0 0 300px;
+  align-items: center;
+  justify-content: center;
+  flex: 0 0 100px;
+  padding: 20px;
   border-top: 2px solid ${({ theme: { border } }): string => border};
   box-shadow: 0 0 5px ${({ theme: { shadow } }): string => shadow};
   background: ${({ theme: { footerTop, footerBottom } }): string =>
     `linear-gradient(${footerTop}, ${footerBottom})`};
+
+  .footer-wrapper {
+    display: flex;
+    flex-direction: column;
+    flex: 0 1 1000px;
+    @media (max-width: 350px) {
+      align-items: center;
+    }
+  }
+
+  .title {
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
+
+  .text-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    @media (max-width: 350px) {
+      justify-content: center;
+    }
+    align-items: center;
+    gap: 10px;
+    font-size: 0.8rem;
+  }
 `;
 
 export default StyledFooter;
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,24 @@ import { memo } from "react";
 import StyledFooter from "./Footer.styles";
 
 const Footer: FC = (): JSX.Element => {
-  return <StyledFooter></StyledFooter>;
+  const smallText = ["v7.0.0", "GPL-3.0", "2022 - 2023", "© Mateusz Perczak"];
+  return (
+    <StyledFooter>
+      <p className="footer-wrapper">
+        <span className="title">Portfolio</span>
+        <span className="text-wrapper">
+          {smallText.map((text, index, textArray) => (
+            <>
+              <span key={`small-text-${index}`}>{text}</span>
+              {textArray.at(-1) !== text && (
+                <span key={`small-text-seperator${index}`}>|</span>
+              )}
+            </>
+          ))}
+        </span>
+      </p>
+    </StyledFooter>
+  );
 };
 
 export default memo(Footer);
