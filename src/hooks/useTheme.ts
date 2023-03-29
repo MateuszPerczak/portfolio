95df268404ea621b39e759eab6e85d7bd098691b
Mateusz Perczak <mateusz.perczak.mp@gmail.com>
2023-03-29 02:31:35 +0200

    Removed react system theme

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,9 +1,26 @@
-import useSystemTheme from "react-use-system-theme";
-import themes, { ColorThemes } from "../themes/themes";
+import { useEffect, useState } from "react";
 
-const useTheme = () => {
-  const systemTheme: ColorThemes = useSystemTheme(ColorThemes.Dark);
-  return themes[systemTheme];
+import themes, { ColorThemes, Theme } from "../themes/themes";
+
+type UpdateThemeProps = MediaQueryListEvent | MediaQueryList;
+
+const useTheme = (): Theme => {
+  const [theme, setTheme] = useState<ColorThemes>(ColorThemes.Dark);
+
+  const updateTheme = ({ matches }: UpdateThemeProps): void => {
+    setTheme(matches ? ColorThemes.Dark : ColorThemes.Light);
+  };
+
+  useEffect(() => {
+    const matchMedia: MediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
+
+    updateTheme(matchMedia);
+
+    matchMedia.addEventListener("change", updateTheme);
+    return () => matchMedia.removeEventListener("change", updateTheme);
+  }, []);
+
+  return themes[theme];
 };
 
 export default useTheme;
